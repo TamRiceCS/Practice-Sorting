f180a8620f3f9de124727a643981ea54f57f0696
TamRiceCS <tam.rice.cs@gmail.com>
2024-01-03 20:16:11 -0600

    Selection, Bubble, Insertion Sorts Done

---
+++ sortingAlgs.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+/*
+    Selection Sort
+        start from the front of the array and find the min val
+        switch min value with start
+        increment start
+        repeat until start = size-1
+*/
+
+void selection(vector<int> nums) {
+    cout << "\nSelection sort (n^2): " << endl;
+
+    int min = 0;
+    for(int i = 0; i < nums.size()-1; i++) {
+        for(int j = i; j < nums.size(); j++) {
+            if(nums[j] <= nums[min]) {
+                min = j;
+            }
+        }
+
+        swap(nums[min], nums[i]);
+        int min = i;
+    }
+
+    for(auto elem : nums) {
+        cout << elem << " ";
+    }
+}
+
+/*
+    Bubble Sort
+        // starting from the front traveling to the end
+        // swap the bigger number "forward"
+        // decrement the end, the last pos is sorted
+*/
+
+void bubble(vector<int> nums){
+
+    cout << "\nBubble sort (n^2): " << endl;
+
+    for(int i = 0; i < nums.size(); i++) {
+        for(int j = i+1; j < nums.size(); j++) {
+            if(nums[i] > nums[j]) {
+                swap(nums[i], nums[j]);
+            }
+        }
+    }
+
+    for(auto elem : nums) {
+        cout << elem << " ";
+    }
+}
+
+/*
+    Insertion Sort
+        Iter through elems, compare it w/ the prior val
+        Swap down curr index until it is no longer the smallest val or reaches start
+          compare starts at i and will decrement by 1 for every swap, also have curr index follow the value you swapped down.
+*/
+void insertion(vector<int> nums) {
+    cout << "\nInsertion sort (n^2): " << endl;
+
+
+    for(int i = 0; i < nums.size(); i++) {
+        int compare = i;
+        for(int j = i - 1; j > -1; j--) {
+            if(nums[compare] < nums[j]) {
+                swap(nums[compare], nums[j]);
+                compare--;
+            }
+        }
+    }
+
+    for(auto elem : nums) {
+        cout << elem << " ";
+    }
+}
+
+void merge(vector<int> nums) {
+
+}
+
+int main() {
+
+    cout << "Let's practice coding sorting algorithms..." << endl;
+
+    cout << "Built in sort (n log n): " << endl;
+
+    vector<int> nums = {10, 8 , 7, 6, 3, 4, 2, 1, 5, 9};
+    sort(nums.begin(), nums.end());
+
+    for(auto elem : nums) {
+        cout << elem << " ";
+    }
+
+    nums = {10, 8 , 7, 6, 3, 4, 2, 1, 5, 9};
+
+    selection(nums);
+    bubble(nums);
+    insertion(nums);
+}
